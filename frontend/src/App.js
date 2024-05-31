13f6a4fec3f59f93d8a59853620294c232ee8d8e
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2024-05-31 12:22:09 -0700

    Routes, Colors, and NavBar
    Created routing with react router dom.
    
    Added root colors for site theme.
    
    Created navbar component. The colors are wrong/need more thought.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './pages/Home';
+import About from './pages/About';
+import NavBar from './components/NavBar'
 
 function App() {
   return (
-    <></>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/components/NavBar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './NavBar.css';
+
+function NavBar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-title">My App</div>
+      <button className="navbar-menu-button">Menu</button>
+    </nav>
+  );
+}
+
+export default NavBar;
