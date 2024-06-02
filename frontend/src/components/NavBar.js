17ccf97926f2d4e18099fd3ba70b3efe1de60e81
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2024-06-01 22:24:50 -0700

    Update NavBar.js

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <nav className="navbar">
       <button className="navbar-menu-button">&#9776;</button> {/* Unicode for hamburger menu icon */}
-      <div className="navbar-title">Korus' Corner</div>
+      <div className="navbar-title">PK Schoolbus</div>
     </nav>
   );
 }
