8403aea2da2f757ed764454d7c4b3d32fdf1dcdb
Caleb Stewart <calebstew32@hotmail.com>
2024-06-21 15:06:14 -0700

    Added vertical hamburger menu
    Does this make it a hotdog menu then?

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './NavBar.css';
+import {Link} from "react-router-dom";
 
 function NavBar() {
-  return (
-    <nav className="navbar">
-      <button className="navbar-menu-button">&#9776;</button> {/* Unicode for hamburger menu icon */}
-      <div className="navbar-title">PK Schoolbus</div>
-    </nav>
-  );
-}
 
-export default NavBar;
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    }
+
+    return (
+      <div>
+          <nav className="navbar ">
+              <button className="navbar-menu-button" onClick={toggleMenu}>&#9776;</button>
+              <div><Link to="/" className="navbar-title">PK Schoolbus</Link></div>
+              <div className={`sidebar ${menuOpen ? 'open' : ''}`}>
+                  <a href="/" className="sidebar-item">Home</a>
+                  <a href="/about" className="sidebar-item">About</a>
+                  {/* Add new links here :D */}
+              </div>
+          </nav>
+          <div className="spacer"></div>
+      </div>
+    ); }
+
+    export default NavBar;
