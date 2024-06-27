4ab378b8d91da6841feba73472e7348912ae46ea
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2024-06-26 21:59:58 -0700

    localize w3

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,27 +4,26 @@ import Home from './pages/Home/Home';
 import About from './pages/About/About';
 import NavBar from './components/NavBar'
 import ArtGallery from './pages/ArtGallery'
-import {useState} from "react";
-
+import { useState } from "react";
+import './w3.css'
 
 function App() {
 
-    const [menuOpened, setMenuOpened] = useState(false);
-    const spaceType = () => {
-        return menuOpened ? "spaceclosed" : "spaceopen";
-    };
+  const [menuOpened, setMenuOpened] = useState(false);
+  const spaceType = () => {
+    return menuOpened ? "spaceclosed" : "spaceopen";
+  };
   return (
     <>
-      <NavBar menuOpened={menuOpened} setMenuOpened={setMenuOpened}/>
+      <NavBar menuOpened={menuOpened} setMenuOpened={setMenuOpened} />
       <div className={spaceType()}>
-    <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/art-gallery' element={<ArtGallery />} />
-        <Route />
-
-      </Routes>
-        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/art-gallery' element={<ArtGallery />} />
+          <Route />
+        </Routes>
+      </div>
     </>
   );
 }
