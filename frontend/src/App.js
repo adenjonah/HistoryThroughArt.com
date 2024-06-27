bed1c03ff92866291f02e7b469865fd1e11179b2
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2024-06-26 21:47:53 -0700

    paths

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
-import About from './pages/About';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
 import NavBar from './components/NavBar'
 import ArtGallery from './pages/ArtGallery'
 import {useState} from "react";
