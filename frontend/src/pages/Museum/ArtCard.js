d92b53a6a0be9859fc4c8ce2454f1f128cc8b224
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2024-07-14 16:12:13 -0700

    Simple refactor of artcard
    Going to change naming conventions and css soon

--- frontend/src/pages/Museum/ArtCard.js
+++ frontend/src/pages/Museum/ArtCard.js
@@ -1,34 +1,21 @@
-import React, {useEffect, useState} from 'react'
-import {useNavigate} from "react-router-dom";
+import React, {useEffect, useState} from 'react';
+import Card from './Card';
 
-const images = require.context('../../artImages', false, /\.png$/);
-
-function ArtCard( {artPiecesArray, search, setArtPiecesArray}) {
-
-    const navigate = useNavigate();
+function ArtCard({artPiecesArray, search, setArtPiecesArray}) {
 
     const [imagesArray, setImagesArray] = useState([]);
 
-    const getImagePath = (imageName) => {
-        try {
-            return images(`./${imageName}`);
-        } catch (e) {
-            console.error(`Cannot find image: ${imageName}`);
-            return '';
-        }
-    };
-
-  useEffect(() => {
-    fetch('http://localhost:5001/museum')
-        .then(response => {
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          return response.json();
-        })
-        .then(data => setArtPiecesArray(data))
-        .catch(error => console.error('Error:', error));
-  }, [setArtPiecesArray]);
+    useEffect(() => {
+        fetch('http://localhost:5001/museum')
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => setArtPiecesArray(data))
+            .catch(error => console.error('Error:', error));
+    }, [setArtPiecesArray]);
 
     useEffect(() => {
         fetch('http://localhost:5001/museum-images')
@@ -42,37 +29,20 @@ function ArtCard( {artPiecesArray, search, setArtPiecesArray}) {
             .catch(error => console.error('Error:', error));
     }, []);
 
+    const filteredArtPieces = artPiecesArray.filter((item) => {
+        return item.name.toLowerCase().includes(search.toLowerCase())
+            || item.artist_culture.toLowerCase().includes(search.toLowerCase())
+            || item.location.toLowerCase().includes(search.toLowerCase())
+            || item.id.toString().toLowerCase().includes(search.toLowerCase());
+    });
 
-  artPiecesArray = artPiecesArray.filter((item) => {
-      return item.name.toLowerCase().includes(search.toLowerCase())
-          || item.artist_culture.toLowerCase().includes(search.toLowerCase())
-          || item.location.toLowerCase().includes(search.toLowerCase())
-          || item.id.toString().toLowerCase().includes(search.toLowerCase());
-  });
-
-  return (
-      <div>
-          {artPiecesArray.map((item, index) => (
-              <div className='w3-panel w3-card artCard w3-hover-shadow w3-hover-opacity' key={index}
-                   onClick={() => navigate(`/exhibit?id=${item.id}`)}>
-                  <div className='column'>
-                      {imagesArray.map((imageItem, index) => {
-                          if (imageItem.id === item.id)
-                              return <img className='images' src={getImagePath(imageItem.image)}
-                                          key={index} alt="nope"></img>
-                          return null;
-                      })}
-                  </div>
-                  <div className='column'>
-                  <h3>{item.name}</h3>
-                  <div>ID: {item.id}</div>
-                  {item.artist_culture !== "None" && <div>Artist/Culture: {item.artist_culture}</div>}
-                  {item.location !== "None" && <div>Location: {item.location}</div>}
-                  <p></p></div>
-              </div>
-          ))}
-      </div>
-  );
+    return (
+        <div>
+            {filteredArtPieces.map((item, index) => (
+                <Card key={index} item={item} imagesArray={imagesArray} />
+            ))}
+        </div>
+    );
 }
 
-export default ArtCard
+export default ArtCard;
