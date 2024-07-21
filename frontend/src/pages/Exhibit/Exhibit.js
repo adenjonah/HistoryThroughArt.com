ae1f7c0133dafc0f4c415f349a56359aedaa6455
Caleb Stewart <calebstew32@hotmail.com>
2024-07-21 13:58:51 -0700

    Reformated date in Exhibit

--- frontend/src/pages/Exhibit/Exhibit.js
+++ frontend/src/pages/Exhibit/Exhibit.js
@@ -24,6 +24,32 @@ function Exhibit() {
             .catch(error => console.error('Error:', error));
     }, [exhibitID]);
 
+
+    const formatDate = (date) => {
+        let dateParts = date.split('/');
+
+        const toBCE = (datePart) => {
+            return datePart.startsWith('-') ? (datePart.slice(1) + ' BCE') : datePart;
+        };
+
+        if (dateParts.length === 2) {
+            dateParts[0] = toBCE(dateParts[0]);
+            dateParts[1] = toBCE(dateParts[1]);
+            dateParts = dateParts.join(' - ');
+        } else {
+            dateParts[0] = toBCE(dateParts[0]);
+            dateParts = dateParts[0];
+        }
+
+        return dateParts;
+    };
+
+    //Displays loading if not loaded yet
+    if (!artPiece) {
+        return <div className="w3-container w3-center"><p>Loading...</p></div>;
+    }
+
+
     return (
         <div className="w3-container">
             <h1 className="w3-center title">{artPiece.name}</h1>
@@ -39,7 +65,7 @@ function Exhibit() {
                     <p className={'blurb'}>Here's some more information on {artPiece.name}:</p>
                     {artPiece.artist_culture !== "None" && <p className={'blurb'}>Artist/Culture: {artPiece.artist_culture}</p>}
                     {artPiece.location !== "None" &&  <p className={'blurb'}>Location Made: {artPiece.location}</p>}
-                    {artPiece.date !== "None" && <p className={'blurb'}>Year Created: {artPiece.date}</p>}
+                    {artPiece.date !== "None" && <p className={'blurb'}>Year Created: {formatDate(artPiece.date)}</p>}
                     {artPiece.materials !== "None" && <p className={'blurb'}>Materials: {artPiece.materials}</p>}
                     <p className={'blurb'}>Unit: {artPiece.unit}</p>
                 </div>
