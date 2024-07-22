ab7b8f28b8954f9c854d132d5c9ad134e73246de
Caleb Stewart <calebstew32@hotmail.com>
2024-07-22 13:24:10 -0700

    Updated ControlBar scalability
    Struggling with increasing the size of the search bar though...

--- frontend/src/pages/Museum/ArtCard.js
+++ frontend/src/pages/Museum/ArtCard.js
@@ -46,11 +46,11 @@ function ArtCard({ item, imagesArray, layout }) {
             className={`w3-card ArtCard w3-hover-shadow w3-hover-purple w3-margin w3-round-xlarge w3-padding-32 ${layout}`}
             onClick={() => navigate(`/exhibit?id=${item.id}`)}>
             <div className="w3-row">
-                <div className="w3-col s12 m5 l4 w3-display-container w3-padding-small grid-lines">
+                <div className="w3-col s12 m5 l4 w3-display-container w3-padding-small">
                     {imageItem &&
                         <img className='w3-image w3-round-xlarge' src={getImagePath(imageItem.image)} alt="Art Piece"/>}
                 </div>
-                <div className="w3-col s12 m6 l8 w3-container grid-lines">
+                <div className="w3-col s12 m6 l8 w3-container">
                     <h3 className="w3-text-theme">{item.id}. {item.name}</h3>
                     <>
                         {item.artist_culture !== "None" &&
