8e6b09b448ef9765e370a5c9df5d1e1f2ec22edf
Caleb Stewart <calebstew32@hotmail.com>
2024-07-26 15:26:51 -0700

    Fixed error

--- frontend/src/pages/Museum/Catalog.js
+++ frontend/src/pages/Museum/Catalog.js
@@ -9,7 +9,7 @@ function Catalog({ search, setArtPiecesArray, layout, sort, unitFilters }) {
     const [fullArtPiecesArray, setFullArtPiecesArray] = useState([]);
     const [artPiecesArray, setLocalArtPiecesArray] = useState([]);
 
-    //Call to get art piece information
+    // Call to get art piece information
     useEffect(() => {
         fetch('http://localhost:5001/museum')
             .then(response => {
@@ -19,15 +19,13 @@ function Catalog({ search, setArtPiecesArray, layout, sort, unitFilters }) {
                 return response.json();
             })
             .then(data => {
-                setFullArtPiecesArray(data)
+                setFullArtPiecesArray(data);
                 setLocalArtPiecesArray(data);
             })
             .catch(error => console.error('Error:', error));
-
-
     }, []);
 
-    //Call to get image names
+    // Call to get image names
     useEffect(() => {
         fetch('http://localhost:5001/museum-images')
             .then(response => {
@@ -40,60 +38,8 @@ function Catalog({ search, setArtPiecesArray, layout, sort, unitFilters }) {
             .catch(error => console.error('Error:', error));
     }, []);
 
-    //Filter out the search from search bar
-    const filteredArtPieces = artPiecesArray.filter((item) => {
-        return item.name.toLowerCase().includes(search.toLowerCase())
-            || item.artist_culture.toLowerCase().includes(search.toLowerCase())
-            || item.location.toLowerCase().includes(search.toLowerCase())
-            || item.id.toString().toLowerCase().includes(search.toLowerCase());
-    });
-    setArtPiecesArray(fullArtPiecesArray);
-
-
-    const itemsPerPage = 50;
-    const startIndex = (currPageNumber - 1) * itemsPerPage;
-    const endIndex = startIndex + itemsPerPage;
-    const currentArtPieces = filteredArtPieces.slice(startIndex, endIndex);
-
-    useEffect(() => {
-        if (search.trim() === '') { //If search is empty
-
-            setCurrPageNumber(prevPageNumber);
-            setIsSearchEmpty(true); //The search is empty
-
-        } else {
-
-            if(isSearchEmpty){ //If search is empty set the previous page number to the current
-                setPrevPageNumber(currPageNumber);
-            }
-            setIsSearchEmpty(false); //Change that the search is full.
-            //We never want to change the prev page number when the search is full.
-
-            setCurrPageNumber(1); //Go to the first page of the searched elements
-        }
-        //This disables a stupid warning:
-        // eslint-disable-next-line
-    }, [search]);
-
-    
-    const handlePageClick = (pageNum) => {
-
-        const scrollTop = () => {
-            window.scrollTo({ top: 0, behavior: 'smooth' });
-        }
-        scrollTop();
-        if(isSearchEmpty) { //If search is empty set previous page number to current page number
-            setPrevPageNumber(currPageNumber);
-        }
-        setCurrPageNumber(pageNum); //Change current page to selected page
-
-
-    };
-
     // Filter art pieces based on search and filters
     useEffect(() => {
-
-        // Filter based on search bar input
         let filteredArtPieces = fullArtPiecesArray.filter((item) => {
             return item.name.toLowerCase().includes(search.toLowerCase())
                 || item.artist_culture.toLowerCase().includes(search.toLowerCase())
@@ -124,7 +70,6 @@ function Catalog({ search, setArtPiecesArray, layout, sort, unitFilters }) {
                 filteredArtPieces.sort((a, b) => a.id > b.id ? 1 : -1);
         }
 
-        // Filter based on unit filters
         if (!Object.values(unitFilters).every(value => value === false)) {
             filteredArtPieces = filteredArtPieces.filter((item) => unitFilters[`unit${item.unit}`]);
         }
@@ -132,35 +77,60 @@ function Catalog({ search, setArtPiecesArray, layout, sort, unitFilters }) {
         setLocalArtPiecesArray(filteredArtPieces);
         setArtPiecesArray(filteredArtPieces);
 
-        if(currPageNumber > Math.ceil(filteredArtPieces.length / itemsPerPage)){
+        if (currPageNumber > Math.ceil(filteredArtPieces.length / itemsPerPage)) {
             setCurrPageNumber(1);
         }
-
         // eslint-disable-next-line
     }, [search, sort, unitFilters, fullArtPiecesArray]);
 
+    // Handle page click
+    const handlePageClick = (pageNum) => {
+        const scrollTop = () => {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+        }
+        scrollTop();
+        if (isSearchEmpty) { // If search is empty set previous page number to current page number
+            setPrevPageNumber(currPageNumber);
+        }
+        setCurrPageNumber(pageNum); // Change current page to selected page
+    };
+
+    // Handle page number update on search input
+    useEffect(() => {
+        if (search.trim() === '') { // If search is empty
+            setCurrPageNumber(prevPageNumber);
+            setIsSearchEmpty(true); // The search is empty
+        } else {
+            if (isSearchEmpty) { // If search is empty set the previous page number to the current
+                setPrevPageNumber(currPageNumber);
+            }
+            setIsSearchEmpty(false); // Change that the search is full.
+            setCurrPageNumber(1); // Go to the first page of the searched elements
+        }
+        // eslint-disable-next-line
+    }, [search]);
+
+    const itemsPerPage = 50;
+    const startIndex = (currPageNumber - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    const currentArtPieces = artPiecesArray.slice(startIndex, endIndex);
 
     return (
         <div>
             <div className={`catalog ${layout}`}>
                 {currentArtPieces.map((item, index) => (
-                    <Card key={index} className={`artCard ${layout}`} item={item} imagesArray={imagesArray}
-                          layout={layout}/>
+                    <Card key={index} className={`artCard ${layout}`} item={item} imagesArray={imagesArray} layout={layout} />
                 ))}
-
-                {filteredArtPieces.length === 0 &&
-                    <h3>No results found</h3>} {/*If no results are found display this message*/}
+                {artPiecesArray.length === 0 && <h3>No results found</h3>} {/* If no results are found display this message */}
             </div>
             <div className="w3-bar">
-                {[...Array(Math.ceil(filteredArtPieces.length / itemsPerPage)).keys()].map(pageNum => ( //This is confusing :|
-                    <a key={pageNum} href={`#${pageNum + 1}`}
-                       className={`w3-button w3-margin-left ${currPageNumber === pageNum + 1 ? "w3-blue" : "w3-light-gray"}`} //Change color of selected page number here
-                       onClick={() => handlePageClick(pageNum + 1)}>
+                {[...Array(Math.ceil(artPiecesArray.length / itemsPerPage)).keys()].map(pageNum => (
+                    <a key={pageNum} href={`#${pageNum + 1}`} className={`w3-button w3-margin-left ${currPageNumber === pageNum + 1 ? "w3-blue" : "w3-light-gray"}`} onClick={() => handlePageClick(pageNum + 1)}>
                         {pageNum + 1}
                     </a>
                 ))}
             </div>
-            <br/>
+            <br />
         </div>
     );
 }
