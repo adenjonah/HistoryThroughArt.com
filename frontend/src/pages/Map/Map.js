225b328888b3c146ba295d90808caa3044f88f70
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2024-09-01 16:11:27 -0400

    Swapped the default to Originated

--- frontend/src/pages/Map/Map.js
+++ frontend/src/pages/Map/Map.js
@@ -3,37 +3,34 @@ import MapBox from './MapBox';
 import './Map.css';
 
 function Map() {
-  const [mapType, setMapType] = useState('currentlyDisplayed');
+  const [mapType, setMapType] = useState('originated'); // Default to 'originated'
 
-    const handleMapToggle = () => {
-
-        if(mapType === 'currentlyDisplayed'){
-            setMapType('originated');
-        }
-        else{
-            setMapType('currentlyDisplayed');
-        }
+  const handleMapToggle = () => {
+    if (mapType === 'originated') {
+      setMapType('currentlyDisplayed');
+    } else {
+      setMapType('originated');
     }
+  };
 
   return (
-      <div className='map pagecontainer'>
-          <h1 className="title">Map</h1>
-          <p className='blurb'>
-              Where the 250 art
-              pieces {mapType === 'currentlyDisplayed' ? "are currently displayed" : "originated from"}
-          </p>
-          <button onClick={handleMapToggle}>
-              Toggle Currently Displayed/Originated
-          </button>
+    <div className="map pagecontainer">
+      <h1 className="title">Map</h1>
+      <p className="blurb">
+        Where the 250 art pieces {mapType === 'currentlyDisplayed' ? 'are currently displayed' : 'originated from'}
+      </p>
+      <button onClick={handleMapToggle}>
+        Toggle Currently Displayed/Originated
+      </button>
 
-          {/* MapBox component */}
-          <div className='w3-display-container' style={{width: '80%', maxWidth: '100%'}}>
-              <MapBox mapType={mapType}/>
-              <div className='w3-display-topleft w3-padding w3-marginleft w3-large'>
-                  {mapType === 'currentlyDisplayed' ? "Currently Displayed" : "Originated"}
-              </div>
-          </div>
+      {/* MapBox component */}
+      <div className="w3-display-container" style={{ width: '80%', maxWidth: '100%' }}>
+        <MapBox mapType={mapType} />
+        <div className="w3-display-topleft w3-padding w3-marginleft w3-large">
+          {mapType === 'currentlyDisplayed' ? 'Currently Displayed' : 'Originated'}
+        </div>
       </div>
+    </div>
   );
 }
 
