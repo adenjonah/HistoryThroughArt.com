cc73d140e777a7b115ee65931ca6eb1f5cce9a16
Jonah Aden <151240959+adenjonah@users.noreply.github.com>
2024-12-03 18:59:25 -0500

    fixed uncaught runtime error
    also fix #100

--- frontend/src/pages/Exhibit/VideoPlayer.js
+++ frontend/src/pages/Exhibit/VideoPlayer.js
@@ -19,12 +19,16 @@ function VideoPlayer({ id }) {
     firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
     window.onYouTubeIframeAPIReady = () => {
-      playerRef.current = new window.YT.Player(iframeRef.current, {
-        events: {
-          onStateChange: onPlayerStateChange,
-          onReady: onPlayerReady,
-        },
-      });
+      if (iframeRef.current) {
+        playerRef.current = new window.YT.Player(iframeRef.current, {
+          events: {
+            onStateChange: onPlayerStateChange,
+            onReady: onPlayerReady,
+          },
+        });
+      } else {
+        console.error("iframeRef.current is not available");
+      }
     };
 
     return () => {
@@ -43,8 +47,7 @@ function VideoPlayer({ id }) {
     }, 500);
   };
 
-  const onPlayerStateChange = (event) => {
-  };
+  const onPlayerStateChange = (event) => {};
 
   useEffect(() => {
     const foundArtPiece = artPiecesData.find(
@@ -123,8 +126,8 @@ function VideoPlayer({ id }) {
   }, [currentTime, visibleTranscript, getActiveTranscriptIndex]);
 
   return (
-    <div className="w3-container">
-      {artVideos.length > 0 && (
+    artVideos.length > 0 && (
+      <div className="w3-container">
         <div className="w3-row">
           <div
             className={`w3-col s12 ${
@@ -195,26 +198,26 @@ function VideoPlayer({ id }) {
             </div>
           )}
         </div>
-      )}
-      {artVideos.length > 1 && (
-        <div className="w3-container w3-center w3-margin-top">
-          {artVideos.map((video, index) => (
-            <button
-              key={index}
-              onClick={() => handleVideoSelection(index)}
-              className={`w3-button w3-ripple w3-bar-item w3-margin-right w3-margin-bottom
+        {artVideos.length > 1 && (
+          <div className="w3-container w3-center w3-margin-top">
+            {artVideos.map((video, index) => (
+              <button
+                key={index}
+                onClick={() => handleVideoSelection(index)}
+                className={`w3-button w3-ripple w3-bar-item w3-margin-right w3-margin-bottom
                                  ${
                                    index === selectedVideo
                                      ? "w3-blue"
                                      : "w3-light-gray"
                                  } `}
-            >
-              Video {index + 1}
-            </button>
-          ))}
-        </div>
-      )}
-    </div>
+              >
+                Video {index + 1}
+              </button>
+            ))}
+          </div>
+        )}
+      </div>
+    )
   );
 }
 
